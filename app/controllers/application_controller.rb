6b0c5f448ee493953e93da40b044486d87c07158
reinaldocosta <reinaldo.costa@gentrop.com>
2023-07-05 18:50:53 -0300

    scaffold products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  set_current_tenant_by_subdomain_or_domain(:tenant, :subdomain, :domain)
   impersonates :user
   include Pundit::Authorization
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_211105) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_05_212042) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -71,6 +71,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_211105) do
     t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
   end
 
+  create_table "products", force: :cascade do |t|
+    t.integer "tenant_id", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tenant_id"], name: "index_products_on_tenant_id"
+  end
+
   create_table "services", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "provider"
@@ -111,5 +119,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_211105) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "products", "tenants"
   add_foreign_key "services", "users"
 end
